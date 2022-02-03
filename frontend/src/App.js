39c1e474e1350e92fc1acb21de245c3e97254a2d
Ana Rita Dias <93427884+anaritadiass@users.noreply.github.com>
2022-02-03 20:09:08 +0000

    Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,27 @@
 //import logo from './logo.svg';
 // import './App.css';
-import store from './components/Store'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './components/Interface/LandingPage'
+import styles from './components/Interface/LandingPage.css'
+import { useState } from 'react'
+
 
 function App() {
+  //const[sideToggle, setSideToggle] = useState(false)
   return (
+ 
     <div className="App">
-     <store />
-     <LandingPage/>
+   <LandingPage />
+     
+     <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ LandingPage} />
+          
+        </Routes>
+
+      </BrowserRouter>
+      
     </div>
   );
 }
--- frontend/src/components/Interface/LandingPage.js
+++ frontend/src/components/Interface/LandingPage.js
@@ -4,16 +4,24 @@ import { useNavigate } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom"
 import { useRef } from 'react';
 
+
 export default function LandingPage() {
           //let navigate = useNavigate();
      
     return (
-        <div>
-        <h1>TODLERS</h1>
-        </div>
-    
+        <div className="navbar">  
+        <button className="Menu">New Born</button>
+       <button className="Menu">Baby</button>
+       <button className="Menu">Child</button>
+        <div className="imagem">
+        <img  width="120" src="/toddlersLogo.svg" />
+      </div>
+      <div className="Login">
+        <button>Login</button>
+        <img width="20px" src="https://cdn-icons-png.flaticon.com/512/126/126510.png"></img>
+     
+  </div>
+  </div>     
 
-
-
-
-    )}
+     );
+    }
