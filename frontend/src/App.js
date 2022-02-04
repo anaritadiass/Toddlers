337d1ab22d10da384c52f1056a38a55bc13fd3f1
Ângela Monteiro Lopes <96127757+AngelaMLopes@users.noreply.github.com>
2022-02-04 05:14:46 +0000

    App.js - alt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,14 @@
 
 import { Login } from './Login';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+//import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // import './components/LandingPage.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './components/Interface/LandingPage'
 import LandingPage from './components/Interface/LandingPage'
 import Baby from './components/Interface/Baby'
-import NewBorn from './components/Interface/NewBorn'
+import NewBorn from './components/Interface/Categories pages newborn/NewBorn'
 import Child from './components/Interface/Child'
-import styles from './components/Interface/LandingPage.css'
 import { useState } from 'react'
 import SignUp from './components/Interface/SignUp'
 
@@ -21,6 +22,16 @@ function App() {
   return (
     <div className="App">
      <Login/>
+   <LandingPage />     
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ LandingPage} />
+          <Route path="/newborn" element={NewBorn} />
+          <Route path="/baby" element={Baby} />
+          <Route path="/child" element={Child} />
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
